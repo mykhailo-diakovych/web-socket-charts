682e3a888e4c610b8770274d73debe3a9a3b723d
mykhailo <m.diakovych13@gmail.com>
2022-10-11 18:56:19 +0300

    add 2 svg, now can swtich line graphic to candlestick graphic.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <CryptoChart />
+<!--  <CandleStickChart />-->
 </template>
 <script>
 
 import CryptoChart from "@/componetns/CryptoChart";
+// import CandleStickChart from "@/componetns/CandleStickChart";
 export default {
   components: {
     CryptoChart,
+    // CandleStickChart
   }
 }
 </script>
